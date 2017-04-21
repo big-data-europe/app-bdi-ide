acbf9be7c603ff9fbd0b968e5ecca464d0b2516f
Ivan Ermilov <earthquakesan@gmail.com>
2017-04-21 14:39:19 +0200

    Initial version of the integrated interface

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BDI-Stack
+
+This application integrates [Stack Builder](https://github.com/big-data-europe/app-stack-builder), [Pipeline Builder](https://github.com/big-data-europe/app-pipeline-builder) and [Swarm UI](https://github.com/big-data-europe/app-swarm-ui) into one interface.
+```
+docker-compose build
+docker-compose up -d
+```
+Navigate to http://localhost/ to see the interfaces.
---
+++ config/resources/domain.lisp
@@ -0,0 +1,90 @@
+(in-package :mu-cl-resources)
+
+;;; Stack Builder
+
+(define-resource docker-compose ()
+  :class (s-prefix "stackbuilder:DockerCompose")
+  :properties `((:text :string ,(s-prefix "stackbuilder:text"))
+                (:title :string ,(s-prefix "dct:title")))
+  :resource-base (s-url "http://stack-builder.big-data-europe.eu/resources/docker-composes/")
+  :on-path "docker-composes")
+
+
+(define-resource container-item ()
+  :class (s-prefix "stackbuilder:ContainerItem")
+  :properties `((:title :string ,(s-prefix "dct:title"))
+                (:repository :string ,(s-prefix "stackbuilder:repository"))
+                (:docker-text :string ,(s-prefix "stackbuilder:dockerText")))
+  :has-many `((container-relation :via ,(s-prefix "stackbuilder:relatedContainer")
+                                :inverse t
+                                :as "relations"))
+  :resource-base (s-url "http://stack-builder.big-data-europe.eu/resources/container-items/")
+  :on-path "container-items")
+
+(define-resource container-group ()
+  :class (s-prefix "stackbuilder:ContainerGroup")
+  :properties `((:title :string ,(s-prefix "dct:title")))
+  :has-many `((container-relation :via ,(s-prefix "stackbuilder:relatedGroup")
+                                :inverse t
+                                :as "relations"))
+  :resource-base (s-url "http://stack-builder.big-data-europe.eu/resources/container-groups/")
+  :on-path "container-groups")
+
+(define-resource container-relation ()
+  :class (s-prefix "stackbuilder:ContainerRelation")
+  :properties `((:index :string ,(s-prefix "stackbuilder:index")))
+  :has-one `((container-group :via ,(s-prefix "stackbuilder:relatedGroup")
+                                :as "group")
+              (container-item :via ,(s-prefix "stackbuilder:relatedContainer")
+                                            :as "item"))
+  :resource-base (s-url "http://stack-builder.big-data-europe.eu/resources/container-relations/")
+  :on-path "container-relations")
+
+;;; Swarm UI
+
+(define-resource repository ()
+  :class (s-prefix "doap:GitRepository")
+  :properties `((:location :string ,(s-prefix "doap:location"))
+                (:title :string ,(s-prefix "dct:title"))
+                (:icon :string ,(s-prefix "w3vocab:icon")))
+  :has-many `((pipeline-instance :via ,(s-prefix "swarmui:pipelines")
+                                 :as "pipeline-instances"))
+  :resource-base (s-url "http://swarm-ui.big-data-europe.eu/resources/repositories/")
+  :on-path "repositories")
+
+(define-resource pipeline-instance ()
+  :class (s-prefix "swarmui:Pipeline")
+  :properties `((:title :string ,(s-prefix "dct:title"))
+                (:icon :string ,(s-prefix "w3vocab:icon"))
+                (:restart-requested :string ,(s-prefix "swarmui:restartRequested")))
+  :has-one `((repository :via ,(s-prefix "swarmui:pipelines")
+                         :inverse t
+                         :as "repository")
+             (status :via ,(s-prefix "swarmui:status")
+                     :as "status")
+             (status :via ,(s-prefix "swarmui:requestedStatus")
+                     :as "requested-status"))
+  :has-many `((service :via ,(s-prefix "swarmui:services")
+                       :as "services"))
+  :resource-base (s-url "http://swarm-ui.big-data-europe.eu/resources/pipeline-instances/")
+  :on-path "pipeline-instances")
+
+(define-resource service ()
+  :class (s-prefix "swarmui:Service")
+  :properties `((:name :string ,(s-prefix "dct:title"))
+                (:scaling :number ,(s-prefix "swarmui:scaling"))
+                (:restart-requested :string ,(s-prefix "swarmui:restartRequested")))
+  :has-one `((pipeline-instance :via ,(s-prefix "swarmui:services")
+                                :inverse t
+                                :as "pipeline-instance"))
+  :resource-base (s-url "http://swarm-ui.big-data-europe.eu/resources/services/")
+  :on-path "services")
+
+(define-resource status ()
+  :class (s-prefix "swarmui:Status")
+  :properties `((:title :string ,(s-prefix "dct:title")))
+  :has-many `((pipeline-instance :via ,(s-prefix "swarmui:status")
+                                 :inverse t
+                                 :as "pipeline-instances"))
+  :resource-base (s-url "http://swarm-ui.big-data-europe.eu/resources/statuses/")
+  :on-path "statuses")
