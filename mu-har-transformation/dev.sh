536f2fad5cc0908d3bc4577b2c5c219f3d3bda3b
Esteban <esteban.sastre@tenforce.com>
2017-05-15 10:59:11 +0200

    Bumped all submodules

--- mu-har-transformation/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it \
-           -v "$PWD"/src:/app/src/ \
-           -v "$PWD"/pcap:/app/pcap \
-           -v "$PWD"/har:/app/har \
-           -v "$PWD"/docker-compose.yml:/app/docker-compose.yml \
-           --name mu-har-transformation \
-           mu-har-transformation
--- mu-swarm-logger-service/docs/README.md
+++
@@ -1,3 +0,0 @@
-# Documentation for the event logger
-* [Events ontology](./docker-engine-events/events.owl)
-* [Possible swarm events for a container lifecycle](./docker-engine-events/container-lifecycle.md)
