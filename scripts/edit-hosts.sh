9ff66ad773c48213dc3496dae3c61dde4bd81474
Arnaud SJongers <asjongers@gmail.com>
2017-09-18 16:56:09 +0200

    added missing host configuration for integrator-ui

--- scripts/edit-hosts.sh
+++ scripts/edit-hosts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+echo "127.0.0.1 integrator-ui.big-data-europe.aksw.org" >> /etc/hosts
 echo "127.0.0.1 stack-builder.big-data-europe.aksw.org" >> /etc/hosts
 echo "127.0.0.1 workflow-builder.big-data-europe.aksw.org" >> /etc/hosts
-echo "127.0.0.1 swarm-ui.big-data-europe.aksw.org/" >> /etc/hosts
-echo "127.0.0.1 kibana.big-data-europe.aksw.org/" >> /etc/hosts
+echo "127.0.0.1 swarm-ui.big-data-europe.aksw.org" >> /etc/hosts
+echo "127.0.0.1 kibana.big-data-europe.aksw.org" >> /etc/hosts
