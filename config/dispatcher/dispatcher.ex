11916ebd476f698fe19fa880226ec87e967fb972
Esteban <esteban.sastre@tenforce.com>
2017-10-09 14:27:23 +0200

    Added support for explicit docker-compose handling from the frontend

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -76,6 +76,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://swarm/"
   end
 
+  match "/drchandler/*path" do
+    Proxy.forward conn, path, "http://drc-handler/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
