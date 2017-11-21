18aa6d656738ce155e61480c662d6daebd16e58f
fr0gs <elf_elegua@yahoo.com>
2017-11-21 11:21:30 +0100

    Bumped integrator-ui v1.8.2 & Fixed a bug not creating data/swarm-admin

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,10 @@ current_directory_no_spaces="${current_directory//\ /\\\\\\\ }"
 sed -i -e "s|UNESCAPED_PWD|$current_directory|g" docker-compose.yml
 sed -i -e "s|PWD|\"$current_directory_no_spaces\"|g" docker-compose.yml
 
+# creating data/swarm-admin to prevent the app to crash.
+echo "Manually creating data/swarm-admin to prevent the app to crash."
+mkdir -p data/swarm-admin
+
 # Setting the hostnames
 echo "configuring your localhost"
 ./scripts/edit-hosts.sh
